7716d9efa344017da7b2aaf707133bc88e807060
anton <anton.molyboha@gmail.com>
2018-02-01 12:20:42 -0500

    SqlStore.createTables(): create the data structure in an empty database

--- src/main/scala/me/molyboha/anton/triplestore/data/jdbc/SqlStore.scala
+++ src/main/scala/me/molyboha/anton/triplestore/data/jdbc/SqlStore.scala
@@ -4,7 +4,7 @@ import me.molyboha.anton.triplestore.data.model.{Notion, Relation, Store}
 
 class SqlStore(connectionString: String) extends Store[String] {
   //protected val connection: java.sql.Connection = java.sql.DriverManager.getConnection("jdbc:mariadb://localhost:3306/triplestore_test?user=triplestore");
-  protected val connection: java.sql.Connection = java.sql.DriverManager.getConnection(connectionString);
+  protected val connection: java.sql.Connection = java.sql.DriverManager.getConnection(connectionString)
 
   protected class SqlNotion(val id: Int, val name: Option[String], val asrelation: Option[Int]) extends Notion[String] {
     override val data: Option[String] = name
@@ -98,5 +98,22 @@ object SqlStore {
       assert(resultSet.getInt(1) == 0)
       resultSet.close()
     }
+  }
+
+  def createTables(connection: java.sql.Connection): Unit = {
+    val statement = connection.createStatement()
+    statement.executeUpdate("CREATE TABLE notion " +
+      "(id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(1024), asrelation INTEGER)")
+    statement.executeUpdate("CREATE TABLE relation " +
+      "(id INTEGER AUTO_INCREMENT PRIMARY KEY, " +
+      "subject INTEGER NOT NULL, " +
+      "verb INTEGER NOT NULL, " +
+      "object INTEGER NOT NULL, " +
+      "asnotion INTEGER NOT NULL, " +
+      "FOREIGN KEY (subject) REFERENCES notion (id), " +
+      "FOREIGN KEY (verb) REFERENCES notion (id), " +
+      "FOREIGN KEY (object) REFERENCES notion (id), " +
+      "FOREIGN KEY (asnotion) REFERENCES notion (id) )")
+    statement.executeUpdate("ALTER TABLE notion ADD FOREIGN KEY (asrelation) REFERENCES relation (id)")
   }
 }
