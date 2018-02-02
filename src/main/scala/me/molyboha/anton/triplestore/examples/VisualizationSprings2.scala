123b15462f8cca1b15b85698ae1c23d533213b58
anton <anton.molyboha@gmail.com>
2018-02-02 12:40:41 -0500

    SpringAutoLayout2: auto-layout the view for GraphView2

---
+++ src/main/scala/me/molyboha/anton/triplestore/examples/VisualizationSprings2.scala
@@ -0,0 +1,35 @@
+package me.molyboha.anton.triplestore.examples
+
+import java.awt.Frame
+import java.awt.event.{WindowAdapter, WindowEvent}
+
+import me.molyboha.anton.triplestore.data.InMemoryStore
+import me.molyboha.anton.triplestore.visual.awt.GraphView2
+import me.molyboha.anton.triplestore.visual.awt.layout.SpringAutoLayout2
+
+object VisualizationSprings2 extends App
+{
+  val store = new InMemoryStore[String]
+  val likes = store.notion("Likes")
+  val jas = store.notion("Jas")
+  val milk = store.notion("Milk")
+  store.relation(jas, likes, milk)
+  val george = store.notion("George")
+  val leek = store.notion("Leek")
+  store.relation(george, likes, leek)
+  val sean = store.notion("Sean")
+  val daughter = store.notion("Daughter of")
+  store.relation(jas, daughter, sean)
+
+
+  val frame = new Frame("Triplestore")
+  val view = new GraphView2[String]
+  frame.add(view)
+  frame.addWindowListener(new WindowAdapter {
+    override def windowClosing(e: WindowEvent): Unit = frame.dispose()
+  })
+  frame.setVisible(true)
+  frame.setSize(800, 500)
+
+  java.awt.EventQueue.invokeLater(() => SpringAutoLayout2(store.notions.filter(_.asRelation.isEmpty).toIterable, view))
+}
